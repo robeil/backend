ae478a1dc15d04330303fd94c836e5fd2fb65347
Robeil Aregawi <silukeen1@gmail.com>
2022-10-10 00:00:58 -0500

    added logging and email service

--- src/main/java/com/robeil/backend/controller/ContactController.java
+++ src/main/java/com/robeil/backend/controller/ContactController.java
@@ -2,7 +2,12 @@ package com.robeil.backend.controller;
 
 import com.robeil.backend.entity.Contact;
 import com.robeil.backend.service.ContactService;
+import com.robeil.backend.service.EmailSenderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +19,20 @@ import java.util.List;
 public class ContactController {
     @Autowired
     private ContactService contactService;
+    @Autowired
+    private EmailSenderService senderService;
+
+
+    Logger logger = LoggerFactory.getLogger(ContactController.class);
+
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendEmail(){
+        senderService.sendEmail(
+                "aregawirobeil1@gmail.com",
+                "From portfolio message",
+                "Some one send you message from your portfolio");
+    }
 
     @PostMapping
     public ResponseEntity<Contact>  addNewContact(@RequestBody Contact contact){
@@ -23,13 +42,18 @@ public class ContactController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Contact>  getNewContact(@PathVariable Long id){
+
     var getNewContact =  contactService.getTheNewContact(id);
+        logger.info("Retrieving user :- "  + getNewContact.getFullName());
+        //sending message each time get a new contact
+        sendEmail();
         return ResponseEntity.ok(getNewContact);
     }
 
     @GetMapping
     public ResponseEntity<List<Contact>> getAllContacts(){
         var allContacts = contactService.getAllContacts();
+        logger.info("Retrieving all the users :- "  + allContacts.toArray());
         return ResponseEntity.ok(allContacts);
     }
 }
---
+++ src/main/java/com/robeil/backend/service/EmailSenderService.java
@@ -0,0 +1,27 @@
+package com.robeil.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@Service
+public class EmailSenderService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmail(String toEmail, String subject, String body){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("robeilaregawi1@gmail.com");
+        message.setTo(toEmail);
+        message.setText(body);
+        message.setSubject(subject);
+
+        mailSender.send(message);
+
+        System.out.println("Mail Sent Successfully");
+    }
+
+
+}
